cc0ee1386e1f121e22e36f73505940362066fed5
NoOseGG <hoc751@gmail.com>
2024-04-14 01:17:09 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>Vite with Tailwind CSS</div>
+    </>
+  );
+}
+
+export default App;
